f3f3a349eb38f34c97f3dd672c1ea6bc327455a0
SSSerg05 <ssserg05@gmail.com>
2026-06-05 14:16:54 +0300

    edit css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
-import StatusLabel, {Status} from '@/app/components/status-label' from "./components/status-label";
+import StatusLabel, {Status} from '@/app/components/status-label';
 
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+    <div >
       <main>
         <h1 className="text-xl">Home page Nextjs-HW-01</h1>
         <StatusLabel status={Status.Active}>Active</StatusLabel>
