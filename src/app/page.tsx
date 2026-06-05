ffcf7ff8f00537a800cc46ec35c03f90c8dff156
SSSerg05 <ssserg05@gmail.com>
2026-06-05 13:52:52 +0300

    add status-label component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
+import StatusLabel, {Status} from '@/app/components/status-label' from "./components/status-label";
+
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main>
         <h1 className="text-xl">Home page Nextjs-HW-01</h1>
+        <StatusLabel status={Status.Active}>Active</StatusLabel>
+        <StatusLabel status={Status.NotActive}>Not Active</StatusLabel>
+        <StatusLabel status={Status.Pending}>Pending</StatusLabel>
+        <StatusLabel status={Status.Suspended}>Suspended</StatusLabel>
       </main>
     </div>
   );
