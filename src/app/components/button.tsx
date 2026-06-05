90ad1894fbd7697b5ded2a63269565eda8b9f661
SSSerg05 <ssserg05@gmail.com>
2026-06-05 18:05:33 +0300

    add Formik + client comp Button input-field

---
+++ src/app/components/button.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import React from "react";
+import clsx from "clsx";
+
+export interface ButtonProps 
+  extends Partial<React.ReactHTMLElement<HTMLButtonElement>> {
+  disabled?: boolean;
+}
+
+export default function Button({disabled, ...rest}: ButtonProps) {
+  return (
+    <button 
+      {...rest} 
+      className={clsx("py-2.5 px-5 bg-gray-900 text-base text-center font-mediun rounded",
+        !disabled && 'hover:bg-gray-800 active:bg-gray-950',
+        disabled && 'text-zink-100',
+      )}
+    />
+  )
+}
