558dd7ab0d33456cfe3864a14eb6858d38573e1f
SSSerg05 <ssserg05@gmail.com>
2026-07-02 13:46:16 +0300

    fix

--- src/app/(admin)/companies/layout.tsx
+++ src/app/(admin)/companies/layout.tsx
@@ -3,12 +3,25 @@ import Sidebar from "@/app/components/sidebar";
 
 export interface LayoutProps {
   children: React.ReactNode;
+  header?: React.ReactNode;
+  toolbar?: React.ReactNode;
+  modal?: React.ReactNode;
 }
 
-export default function Layout({ children }: LayoutProps) {
+export default function Layout({ 
+  children,
+  header,
+  toolbar,
+  modal
+}: LayoutProps) {
   return 
     <>
-      <Sidebar />
-      <div className="ml-60">{children}</div>;
+      {modal}
+      {header}
+      <main>
+        {toolbar}
+        <Sidebar />
+        <div className="ml-60">{children}</div>;
+      </main>
     </>
 }
