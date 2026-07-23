be79ea1c357a4cda542c20efc76e582412de80de
SSSerg05 <ssserg05@gmail.com>
2026-07-23 09:38:34 +0300

    add stats widjet

--- src/app/(admin)/dashboard/@stats/page.tsx
+++ src/app/(admin)/dashboard/@stats/page.tsx
@@ -1,13 +1,32 @@
 import React from "react";
+import { getSummaryStats } from "@/lib/api";
+import StatCard, {StatCardType} from "@/app/components/stat-card/stat-card";
 
 export interface PageProps {
   params: '0';
 }
 
-export default function Page({}: PageProps) {
+const labelByStat = {
+  promotions: "Total promotions",
+  categories: "Total categories",
+  newCompanies: "New Companies",
+  activeCompanies: "Total active companies",
+};
+
+export default async function Page({}: PageProps) {
+  const data = await getSummaryStats();
+
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2"> 
- 
+    <div className="grid grid-cols-12 gap-5"> 
+      {(Object.keys(labelByStat) as (keyof typeof data)[]).map((key) => (
+        <div key={key} className="col-span-3">
+          <StatCard 
+            type={StatCardType.Gradient} 
+            label={labelByStat[key]}
+            counter={data[key]}
+          />
+        </div>
+      ))}
     </div>
   );
 }
