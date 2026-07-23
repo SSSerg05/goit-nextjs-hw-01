90efe7a266e364ab65331bbf99b26cb99f5f5112
SSSerg05 <ssserg05@gmail.com>
2026-07-23 10:02:30 +0300

    fix

--- src/app/(admin)/dashboard/page.tsx
+++ src/app/(admin)/dashboard/page.tsx
@@ -7,14 +7,20 @@ export interface PageProps {
 
 export default function Page({}: PageProps) {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
-        <Header>Dashboard</Header>
-        <p className="text-lg text-gray-600 mt-4">
-          Welcome to your dashboard!
-        </p>
+    // <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    //   <main className="flex flex-col items-center justify-center w-full flex-1 px-20 text-center">
+    //     <Header>Dashboard</Header>
+    //     <p className="text-lg text-gray-600 mt-4">
+    //       Welcome to your dashboard!
+    //     </p>
 
-      </main>
-    </div>
+    //   </main>
+    // </div>
+    <>
+      <Header>Dashboard</Header>
+      <p className="text-lg text-gray-600 mt-4">
+        Welcome to your dashboard!
+      </p>
+    </>
   );
 }
